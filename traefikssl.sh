f4644fa1269781fd4370ab04808d74df12291042
Benjamin Isong <beisongabc@gmail.com>
2025-04-27 19:46:55 -0400

    updated with node exporter

--- traefikssl.sh
+++ traefikssl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/traefik-monitor/traefik/letsencrypt
-rm acme.json
-touch acme.json
+cd ~/traefik-monitor/traefik/letsencrypt/ && \
+rm acme.json && \
+touch acme.json && \
 chmod 600 acme.json
